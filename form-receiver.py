476caeddddf78047cb7d9db7a40a712a82eeff4f
Florian <t.florian181181@gmail.com>
2023-10-04 17:34:53 +0200

    04/10/23

--- form-receiver.py
+++ form-receiver.py
@@ -225,7 +225,7 @@ def flask_sendersession():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", debug=False, port=6601)
+    app.run(host="164.132.229.216", debug=False, port=6601)
     load_dotenv(".env")
 
     
