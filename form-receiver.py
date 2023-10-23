a0d9196991197bda1e6bba8713ffed0b538e4979
Florian <t.florian181181@gmail.com>
2023-10-23 16:09:19 +0200

    23/10/23

--- form-receiver.py
+++ form-receiver.py
@@ -243,12 +243,15 @@ def flask_set_fiche_status():
     fiche_id_to_change = request.get_json()
     print("fiche_id_to_change", fiche_id_to_change)
 
-    if check_session_validity(cookies):
-        return jsonify(get_fiches_json_data())
-    else:
-        return jsonify("Des cookies de connexion sont presents mais incorrects, essayez de supprimer les cookies puis entrez le mot de passe.")
+    fiches = json.load(get_fiches_json_data())
 
+    for fiche in fiches:
+        if fiche["id"] == fiche_id_to_change:
+            fiche["status"] = True if fiche["id"] else False
 
+    with open("./fiches-client/fiches.json", "r") as file:
+        file.write(json.dump(fiches))
+        file.close()
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=False, port=6601)
