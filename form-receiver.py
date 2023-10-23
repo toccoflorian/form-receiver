fdd3704bfefaff1eb74836901051a1f00d99d30e
Florian <t.florian181181@gmail.com>
2023-10-23 16:14:37 +0200

    23/10/23

--- form-receiver.py
+++ form-receiver.py
@@ -242,7 +242,7 @@ def flask_set_fiche_status():
     fiche_id_to_change = request.get_json()
 
     fiches = json.dumps(get_fiches_json_data())
-    print(fiches)
+    print(type(fiches))
 
     for fiche in fiches:
         if fiche["id"] == fiche_id_to_change:
