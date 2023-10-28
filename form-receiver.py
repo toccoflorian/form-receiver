589e1b157c98f913d752b0378aaf2c3968be64e6
Florian <t.florian181181@gmail.com>
2023-10-28 13:25:53 +0200

    28/10/23

--- form-receiver.py
+++ form-receiver.py
@@ -40,7 +40,7 @@ def send_data_by_email(data: str, filename: str):
         api_url,
         auth=("api", api_key),
         data={"from": mail_user,
-              "to": 't.florian181181@gmail.com;t.florian181181@gmail.com',
+              "to": 'victor@parlonspc.fr;t.contact@parlonspc.fr',
               "subject": filename,
               "text": data})
 
@@ -239,7 +239,6 @@ def flask_set_fiche_status():
     fiches = json.loads(json_fiches)
 
     for fiche in fiches:
-        # print(fiche)
         if fiche == fiche_id_to_change:
             fiches[fiche]["status"] = False if fiches[fiche]["status"] else True
 
