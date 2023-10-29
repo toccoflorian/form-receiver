9857081b9a3e8ae89381e61c8a5d0bb46ab33717
Florian <t.florian181181@gmail.com>
2023-10-29 19:02:54 +0100

    29/10/23

--- form-receiver.py
+++ form-receiver.py
@@ -40,7 +40,7 @@ def send_data_by_email(data: str, filename: str):
         api_url,
         auth=("api", api_key),
         data={"from": mail_user,
-              "to": 'victor@parlonspc.fr;t.contact@parlonspc.fr',
+              "to": 'victor@parlonspc.fr; contact@parlonspc.fr',
               "subject": filename,
               "text": data})
 
